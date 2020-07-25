d0a6892eacfeafb882e52e6b8353a44ebadfa7e3
Bomyee Woo <bomyeew@gmail.com>
2020-07-25 08:52:33 -0400

    Add mobile menu item toggle function

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-// Hamburger Menu
+// Hamburger Menu Icon Toggle
 var mobileMenu = document.querySelector('.hamburger');
+var menuList = document.querySelector('.menu-links');
 
 function toggleMenu() {
     mobileMenu.classList.toggle('close');
+    menuList.classList.toggle('show');
 }
 
 mobileMenu.addEventListener('click', toggleMenu);
