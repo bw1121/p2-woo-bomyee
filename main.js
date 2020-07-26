005075e1db8db93c335fad6b26549ae944f17424
Bomyee Woo <bomyeew@gmail.com>
2020-07-26 10:30:49 -0400

    Add quotes API

--- main.js
+++ main.js
@@ -76,3 +76,25 @@ function openAccordion() {
 for (i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener('click', openAccordion);   
 }
+
+// Quotes API 
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var apiResult = JSON.parse(this.responseText);
+
+        // Random quote
+        var randQuote = document.getElementById('randQuote');
+        var circle = document.querySelector('.background-circle');
+
+        function showQuote() {
+            randQuote.innerHTML = apiResult.slip.advice;
+            randQuote.style.fontWeight = '400';
+        }
+
+        circle.addEventListener('click', showQuote);
+
+    }
+};
+xmlhttp.open('GET', 'https://api.adviceslip.com/advice', true);
+xmlhttp.send();
